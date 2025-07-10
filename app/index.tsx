3a963eb03c646dd30ce31d3244e2c81736a67a81
Waqar Khan <waqar.mrwt424@gmail.com>
2025-07-10 15:47:54 +0500

    new commit

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">Welcome</Text>
     </View>
   );
 }
