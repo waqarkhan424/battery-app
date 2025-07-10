1aa0bb426d9cff54a419bfc09e2e949f23756e12
Waqar Khan <waqar.mrwt424@gmail.com>
2025-07-10 16:38:58 +0500

    new commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,5 @@
 import { Stack } from "expo-router";
-import "./global.css";
-
+import "./globals.css";
 export default function RootLayout() {
   return <Stack />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,9 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">Welcome</Text>
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome
+      </Text>
     </View>
   );
 }
