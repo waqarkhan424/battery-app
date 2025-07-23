b535a3b57dbca87a58a408abc3d55d98478b8967
Waqar Khan <waqar.mrwt424@gmail.com>
2025-07-23 22:20:20 +0500

    Initial commit

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,35 +1,20 @@
-import { useEvent } from 'expo';
-import { useVideoPlayer, VideoSource, VideoView } from 'expo-video';
+import { ResizeMode, Video } from 'expo-av';
 import { View } from 'react-native';
 
 type Props = { url: string };
 
 export default function VideoCard({ url }: Props) {
-    const player = useVideoPlayer(
-        url as VideoSource,
-        player => {
-            player.loop = true;
-            player.muted = true;
-            player.play();
-        }
-    );
-
-    // Correct payload usage via useEvent
-    const { isPlaying } = useEvent(
-        player,
-        'playingChange',
-        { isPlaying: player.playing }
-    );
-
-    return (
-        <View className="w-32 h-40 rounded-lg overflow-hidden bg-black mr-3">
-            <VideoView
-                player={player}
-                className="w-full h-full"
-                contentFit="cover"
-                allowsFullscreen={false}
-                allowsPictureInPicture={false}
-            />
-        </View>
-    );
+  return (
+    <View className="w-32 h-40 rounded-lg overflow-hidden bg-black mr-3">
+      <Video
+        source={{ uri: url }}
+        style={{ width: '100%', height: '100%' }}
+        resizeMode={ResizeMode.COVER}
+        shouldPlay
+        isLooping
+        isMuted
+        useNativeControls={false}
+      />
+    </View>
+  );
 }
