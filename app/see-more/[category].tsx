290de2abc822d3b0abe9518de0c669ebbbfcd977
Waqar Khan <waqar.mrwt424@gmail.com>
2025-07-26 16:13:09 +0500

    Initial commit

--- app/see-more/[category].tsx
+++ app/see-more/[category].tsx
@@ -2,7 +2,7 @@ import VideoCard from '@/components/video-card';
 import { fetchVideosFromGitHub, VideoItem } from '@/lib/fetch-videos';
 import { useLocalSearchParams } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { Dimensions, FlatList, Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 
 export default function SeeMoreScreen() {
   const { category } = useLocalSearchParams<{ category: string }>();
@@ -14,28 +14,18 @@ export default function SeeMoreScreen() {
     }
   }, [category]);
 
-  const numColumns = 3;
-  const screenWidth = Dimensions.get('window').width;
-  const itemSize = screenWidth / numColumns - 18; // include padding/margin
-
   return (
-    <View className="flex-1 bg-background px-4 pt-6">
-      <Text className="text-white text-2xl font-bold capitalize mb-4 text-center">
+    <ScrollView className="flex-1 bg-background px-4 pt-6">
+      <Text className="text-white text-2xl font-bold capitalize mb-4">
         {category}
       </Text>
-
-      <FlatList
-        data={videos}
-        keyExtractor={(item) => item.id}
-        numColumns={numColumns}
-        contentContainerStyle={{ paddingBottom: 30 }}
-        columnWrapperStyle={{ justifyContent: 'space-between', marginBottom: 12 }}
-        renderItem={({ item }) => (
-          <View style={{ width: itemSize }}>
-            <VideoCard url={item.url} thumbnail={item.thumbnail} />
+      <View className="flex-row flex-wrap justify-start">
+        {videos.map((video) => (
+          <View key={video.id} className="mb-4 mr-3">
+            <VideoCard url={video.url} thumbnail={video.thumbnail} />
           </View>
-        )}
-      />
-    </View>
+        ))}
+      </View>
+    </ScrollView>
   );
 }
