701120048373b2680e741205df533f279d37ec53
Waqar Khan <waqar.mrwt424@gmail.com>
2025-07-26 15:14:27 +0500

    Initial commit

---
+++ app/see-more/[category].tsx
@@ -0,0 +1,31 @@
+import VideoCard from '@/components/video-card';
+import { fetchVideosFromGitHub, VideoItem } from '@/lib/fetch-videos';
+import { useLocalSearchParams } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { ScrollView, Text, View } from 'react-native';
+
+export default function ViewAllScreen() {
+  const { category } = useLocalSearchParams<{ category: string }>();
+  const [videos, setVideos] = useState<VideoItem[]>([]);
+
+  useEffect(() => {
+    if (category) {
+      fetchVideosFromGitHub(category).then(setVideos);
+    }
+  }, [category]);
+
+  return (
+    <ScrollView className="flex-1 bg-background px-4 pt-6">
+      <Text className="text-white text-2xl font-bold capitalize mb-4">
+        {category} Animations
+      </Text>
+      <View className="flex-row flex-wrap justify-start">
+        {videos.map((video) => (
+          <View key={video.id} className="mb-4 mr-3">
+            <VideoCard url={video.url} thumbnail={video.thumbnail} />
+          </View>
+        ))}
+      </View>
+    </ScrollView>
+  );
+}
