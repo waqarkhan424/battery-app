5142c8deafd9915caff3bf4b94541b33929c0fbd
Waqar Khan <waqar.mrwt424@gmail.com>
2025-07-27 23:01:40 +0500

    Initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,17 @@
 import { Stack } from 'expo-router';
 import { configureReanimatedLogger } from 'react-native-reanimated';
+import ChargingListener from './_charging-listener';
 import './global.css';
 
 configureReanimatedLogger({ strict: false });  // ← turn off strict‑mode warnings
 
 export default function RootLayout() {
     return (
-      
+      <>
+
+{/* Charging listener always active */}
+      <ChargingListener /> 
+
   <Stack>
       {/* Hide header for tab screens */}
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
@@ -19,6 +24,6 @@ export default function RootLayout() {
 
 
     </Stack>
-
+</>
     );
 }
