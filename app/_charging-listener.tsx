a9840beed42695076e13a7570bc7fa5b1784d765
Waqar Khan <waqar.mrwt424@gmail.com>
2025-07-27 22:47:13 +0500

    Initial commit

---
+++ app/_charging-listener.tsx
@@ -0,0 +1,27 @@
+import { useSettingsStore } from '@/store/settings';
+import * as Battery from 'expo-battery';
+import { router } from 'expo-router';
+import { useEffect } from 'react';
+
+export default function ChargingListener() {
+  const { enableAnimations, appliedAnimation } = useSettingsStore();
+
+  useEffect(() => {
+    const subscription = Battery.addBatteryStateListener(({ batteryState }) => {
+      if (
+        batteryState === Battery.BatteryState.CHARGING &&
+        enableAnimations &&
+        appliedAnimation
+      ) {
+        router.push({
+          pathname: '/video-player/[videoUrl]',
+          params: { videoUrl: encodeURIComponent(appliedAnimation) },
+        });
+      }
+    });
+
+    return () => subscription.remove();
+  }, [appliedAnimation, enableAnimations]);
+
+  return null;
+}
