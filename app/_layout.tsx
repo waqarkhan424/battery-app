73f9f162c646cfbdeda6a0d0ffd675f07845ffe0
Waqar Khan <waqar.mrwt424@gmail.com>
2025-08-07 16:59:36 +0500

    Initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,23 @@
+ import { useSettingsStore } from '@/store/settings';
 import { Stack } from 'expo-router';
+import { useEffect } from 'react';
+import { NativeModules } from 'react-native';
 import { configureReanimatedLogger } from 'react-native-reanimated';
 import './global.css';
 
 configureReanimatedLogger({ strict: false }); // turn off strict-mode warnings
 
 export default function RootLayout() {
+
+ const { enableAnimations, appliedAnimation } = useSettingsStore();
+
+ useEffect(() => {
+  if (enableAnimations) {
+    NativeModules.ChargingServiceModule.startService(appliedAnimation ?? '');
+  }
+}, [enableAnimations]);
+
+
   return (
     <Stack>
       {/* Hide header for tab screens */}
