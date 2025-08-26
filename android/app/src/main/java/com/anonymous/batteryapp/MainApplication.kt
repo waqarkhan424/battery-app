c7c494f35e8ce8809f47ccfb8fb3bf6e87c99d2f
Waqar Khan <waqar.mrwt424@gmail.com>
2025-08-26 17:38:41 +0500

    Initial commit

--- android/app/src/main/java/com/anonymous/batteryapp/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/batteryapp/MainApplication.kt
@@ -40,6 +40,7 @@ class MainApplication : Application(), ReactApplication {
   override val reactHost: ReactHost
     get() = ReactNativeHostWrapper.createReactHost(applicationContext, reactNativeHost)
 
+
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, OpenSourceMergedSoMapping)
